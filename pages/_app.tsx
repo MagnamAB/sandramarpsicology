6565c35f724a16eae1a198e77e9ed58f452340dd
MagnamAB <anbovar@hotmail.com>
2025-11-03 20:55:54 +0100

    Actualizar favicon con el logo de Sandra Vargas

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -39,7 +39,8 @@ export default function App({ Component, pageProps }: AppProps) {
       <GoogleAnalytics />
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
+        <link rel="apple-touch-icon" href="/favicon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
       </Head>
